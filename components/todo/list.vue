c30e2ba28ae3a82d88a32922f335c6de7d0a8908
Nikita Makarin <n-makarin@mail.ru>
2019-11-26 12:49:21 +0300

    Create components\todo\list.vue

---
+++ components/todo/list.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="todo-list">
+    <todo
+      v-for="todo in data"
+      :key="todo.id"
+      :title="todo.title"
+      :completed="todo.completed"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Todo from '~/components/todo.vue'
+
+export default Vue.extend({
+  components: {
+    Todo
+  },
+  props: {
+    data: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
