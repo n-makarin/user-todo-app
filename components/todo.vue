8b300ecb62423b0e8e271c7bd85304a9d821e80f
Nikita Makarin <n-makarin@mail.ru>
2019-11-26 16:44:34 +0300

    Add styles to components\todo.vue

--- components/todo.vue
+++ components/todo.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="todo">
-    <div class="todo-title">
+    <div class="todo__checkbox">
+      <input id="checkbox" v-model="checked" type="checkbox">
+      <span />
+    </div>
+    <div class="todo__title">
       {{ title }}
     </div>
-    <input id="checkbox" v-model="checked" type="checkbox">
   </div>
 </template>
 
@@ -41,4 +44,59 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
+.todo {
+  position: relative;
+  display: flex;
+  flex-wrap: wrap;
+
+  &__title {
+    &:first-letter {
+      text-transform: uppercase;
+    }
+  }
+  &__checkbox {
+    position: relative;
+    margin-right: 10px;
+    height: 20px;
+    input {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      opacity: 0;
+      z-index: 2;
+      cursor: pointer;
+
+      /* checked */
+      &:checked ~ span {
+        &::before {
+          opacity: 1;
+        }
+      }
+    }
+    span {
+      display: inline-block;
+      width: 15px;
+      height: 15px;
+      border-radius: 3px;
+      border: 1px solid #c5c5c5;
+      z-index: 1;
+
+      &::before {
+        content: '';
+        position: absolute;
+        top: 19%;
+        left: 24%;
+        width: 6px;
+        height: 5px;
+        border-bottom: 2px solid rgb(156, 156, 214);
+        border-left: 2px solid rgb(156, 156, 214);
+        transform: rotate(-45deg);
+        opacity: 0;
+      }
+    }
+    &:hover span {
+      background: #f3f3f3;
+    }
+  }
+}
 </style>
